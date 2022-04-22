e9cd2992f8dac7da4e5b1acaf0f247fd04dcb402
DeadmanLabs <michael.magahey@gmail.com>
2022-04-22 17:31:16 -0400

    Wallet Display and Server Structure

--- chess/src/App.js
+++ chess/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+import { Nav } from './Components/Nav.tsx';
+
 function App() {
+
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]); //We can attach a switch to network to make a mainnet version
+  const wallets = useMemo(() => [
+    new PhantomWalletAdapter(),
+    new SlopeWalletAdapter(),
+    new SolflareWalletAdapter({ network }),
+    new LedgerWalletAdapter(),
+    new SolletWalletAdapter({ network }),
+    new SolletExtensionWalletAdapter({ network })
+  ], [network]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <Nav />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </div>
   );
 }
---
+++ chess/src/Components/Nav.tsx
@@ -0,0 +1,53 @@
+import {
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { useWallet, useConnection } from '@solana/wallet-adapter-react';
+import { WalletNotConnectedError, WalletSignTransactionError } from '@solana/wallet-adapter-base';
+import React, { useState, useCallback, useEffect, useMemo } from 'react';
+import './Nav.css';
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { loadavg } from 'os';
+
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+function Nav(props: any)
+{
+    const { connection } = useConnection();
+    const { publicKey, sendTransaction } = useWallet();
+    let grabBalance = async function() {
+        let balance = 0.0;
+        try {
+            balance = (await connection.getBalance(publicKey)).valueOf() / LAMPORTS_PER_SOL;
+        } catch (e) {
+            console.log("[DBG] - Failed to grab balance inside of NAV!");
+        }
+        return balance.toFixed(8);
+    }
+    const [balance, setBalance] = useState("");
+    useEffect(() => {
+        let active = true
+        load()
+        return () => { active = false }
+
+        async function load() {
+            setBalance("")
+            const res = await grabBalance();
+            if (!active) { return }
+            setBalance(res)
+        }
+    }, [connection, publicKey]);
+    
+    
+
+    return (
+        <div className="nav">
+            <li className="nav-left"></li>
+            <li className="nav-middle"><a><div className="balance-view">{balance} SOL</div></a></li>
+            <li className="nav-right"><a><WalletMultiButton /></a></li>
+            <li className="nav-right"><a><WalletDisconnectButton /></a></li>
+        </div>
+    )
+}
+
+export { Nav };
