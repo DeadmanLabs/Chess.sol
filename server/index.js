e3a5ce69cf135c6f5b3b65a556cb671413296f25
DeadmanLabs <michael.magahey@gmail.com>
2022-04-23 19:31:52 -0400

    Payments and Socket Updates

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 http = require('http'); //Update to https later
 const solana = require('@solana/web3.js');
 const bodyParser = require('body-parser');
+const uuid = require('uuid');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const server = http.createServer(app);
@@ -11,7 +12,7 @@ const wss = require('socket.io')(server, {
         origin: '*',
     }
 });
-const games = []
+const games = {}
 const escrow = solana.Keypair.fromSecretKey(new Uint8Array([83,61,11,202,233,91,145,84,114,246,170,148,104,93,70,122,219,29,
                                                             237,128,185,176,237,178,33,164,177,85,83,48,23,17,187,58,155,176,
                                                             253,28,255,9,247,153,198,53,44,198,150,99,230,59,102,233,118,53,
@@ -29,12 +30,30 @@ class Game {
         this.challenger = undefined;
         this.parent_raw = parent_raw;
         this.challenger_raw = challenger_raw;
+        this.gameover = false;
     }
 
-    payWinner() {
+    async payWinner() {
         if (this.checkWin() != undefined)
         {
-            //pay this.checkWin()
+            const transaction = new web3.Transaction().add(
+                web3.SystemProgram.transfer({
+                    fromPubkey: escrow.publicKey,
+                    toPubkey: this.checkWin(),
+                    lamports: (wager * 2) - 0.00001
+                }),
+            );
+            const signature = await web3.sendAndConfirmTransaction(
+                network,
+                transaction,
+                [escrow]
+            );
+            let result = JSON.stringify({});
+            this.parent_raw.emit(result);
+            this.challenger_raw.emit(result);
+            this.gameover = true;
+            this.parent_raw.close();
+            this.challenger_raw.close();
         }
     }
 
@@ -48,18 +67,20 @@ class Game {
 }
 
 app.get('/games', function (req, res) {
-    let response = {games: []};
-    games.forEach(element => {
-        response.games.push(element.response)
-    });
+    let response = {games: {}};
+    for (const [key, value] of Object.entries(games)) {
+        response.games[key] = games[key].toJSON();
+    }
     res.send(JSON.stringify(response));
     res.end();
 });
 
 app.post('/new', function (req, res) {
     let params = JSON.parse(req.body.params);
-    games.push(new Game(params.wager, params.password, params.parent, params.parent_raw));
-    res.send(JSON.stringify({ status: "success" }))
+    let id = uuid.v4();
+    games[id] = new Game(params.wager, params.password, params.parent, params.parent_raw);
+    res.send(JSON.stringify({ status: "success", game: id }));
+    res.end();
 });
 
 server.listen(80); //change to 443 for HTTPS
@@ -73,7 +94,29 @@ async function sleep(ms) {
 wss.on('connection', async (ws) => {
     ws.on('join', (data) => {
         let params = JSON.parse(data);
+        if (games[params.id] != undefined)
+        {
+            if (games[params.id].password == params.password) 
+            {
+                games[params.id].challenger = params.address;
+                games[params.id].challenger_raw = ws;
+                ws.on('', (data) => {
 
+                });
+                ws.on('', (data) => {
+
+                });
+                ws.emit(JSON.stringify({ status: "success", reason: "", code: 200 }));
+            }
+            else 
+            {
+                ws.emit(JSON.stringify({ status: "failed", reason: "Password is invalid!", code: 403 }));
+            }
+        }
+        else 
+        {
+            ws.emit(JSON.stringify({ status: "failed", reason: "Requested game does not exist!", code: 404 }));
+        }
     });
 });
 
